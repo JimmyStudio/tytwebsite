1f6be43d4e795494e267803dce7e1d788007ad5a
Jimmy <Jimmystudio@yeah.net>
2018-06-12 11:22:55 +0800

    team

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -4,147 +4,142 @@
 <template>
   <div class="main">
     <div class="content">
-      <div class="sec">TEAM MEMBER</div>
+      <div class="sec">团队</div>
       <div class="member">
         <div class="name">Hanson Huang</div>
-        <div class="title">Founder/Former Tencent Employee</div>
+        <div class="title">创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Over 15 years of experience in Marketing, Product design & Investment</p>
-        <p class="desc">• Joined Tencent at 2009, worked in the Enterprise Products Division, Wireless Products Division, and Tencent Research Institute</p>
-        <p class="desc">• Served as the Director of Marketing of Tencent Research Institute before leaving Tecent at 2009</p>
-        <p class="desc">• Tencent's first M3 rank employee</p>
-        <p class="desc">• Served in the board of directors for several IT companies since 2010</p>
-        <p class="desc">• Founded TYT in 2017</p>
+        <p class="desc">• 超过15年产品、营销、投资经验</p>
+        <p class="desc">• 2004-09入职腾讯，历经企业产品部，无线产品部，腾讯研究院，曾负责公司级企业产品和无线终端业务渠道和市场业务，离职时担任腾讯研究院市场总监,05年腾讯首批职级M3级核心骨干</p>
+        <p class="desc">• 2010年-2017年担任数家互联网科技公司董事</p>
+        <p class="desc">• 2017年创办 TYT</p>
       </div>
       <div class="member">
         <div class="name">Jie Song</div>
-        <div class="title">Co-Founder</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Held dual bachelor’s degrees in Finance and Math from Indiana University</p>
-        <p class="desc">• Held a master’s degree in Statistics from Rice University</p>
-        <p class="desc">• Worked at Yongan Futures Co. as analyst</p>
-        <p class="desc">• Has over 5 years of experience in machine learning, data mining, artificial intelligence algorithms, quantitative finance, and blockchain architecture</p>
-        <p class="desc">• Currently responsible for R&D in TYT</p>
+        <p class="desc">• 美国印第安纳大学金融、数学双学士学位；美国莱斯大学统计学硕士学位，在校担任助教，研究员等工作。通过北美精算师协会两门精算师考试</p>
+        <p class="desc">• 曾参与哈佛大学区块链小组研发工作</p>
+        <p class="desc">• 回国后在永安期货任研究员，负责量化交易模型研究以及交易系统搭建</p>
+        <p class="desc">• 熟悉多种计算机语言，对人工智能，机器学习以及区块链底层技术有深入的研究</p>
+        <p class="desc">• 目前负责 TYT 整体研究和开发工作</p>
       </div>
       <div class="member">
         <div class="name">Yvonne Wu</div>
-        <div class="title">Co-Founder/Former Tencent Employee</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Former head of BD in Financial Businesses at ZhongAn Technology</p>
-        <p class="desc">• Over 15 years of business development experience in Communications, E-Commerce, and Fin-tech</p>
-        <p class="desc">• Have a profound understanding in blockchain operational concept</p>
-        <p class="desc">• Worked at China Mobile, Motorola, GaoPeng, Tencent, Paypal, China PingAn</p>
-        <p class="desc">• Held a joint MBA degree from Columbia University & Hongkong University</p>
-        <p class="desc">• Held a bechelor’s degree in economics from Fudan University</p>
-        <p class="desc">• Currently responsible for business development in TYT</p>
+        <p class="desc">• 原众安科技金融业务线商务负责人</p>
+        <p class="desc">• 在通信、电子商务以及互联网金融有超过15年的业务拓展经验</p>
+        <p class="desc">• 曾在中国移动、摩托罗拉、高朋、腾讯、PayPal、中国平安及众安科技等任职</p>
+        <p class="desc">• 香港大学&哥伦比亚大学联合项目MBA、复旦大学世界经济系学士、目前清华五道口互联网金融进修班</p>
+        <p class="desc">• 深刻了解目前区块链科技现状和前沿运营理念</p>
       </div>
       <div class="member">
         <div class="name">Yang Liang</div>
-        <div class="title">Co-Founder/Former Tencent Employee</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Former CEO of LanYou online, CPO of Beijing Donglingxili</p>
-        <p class="desc">• Worked at Tencent Interactive Entertainment</p>
-        <p class="desc">• Co-founded ‘FangShangFang’ technology with DAU over 2 million</p>
-        <p class="desc">• Operated online product with annual net income over 100 million</p>
-        <p class="desc">• Series entrepreneur, LanYou online was successfully acquired</p>
+        <p class="desc">• 曾联合创建“房上房”科技的互联网管控系统，活跃用户超过200万</p>
+        <p class="desc">• 操作过年净利润1亿+的互联网产品</p>
+        <p class="desc">• 曾任职腾讯互娱，北京东邻西里CPO和岚游在线CEO</p>
+        <p class="desc">• 连续创业者，创建的岚游在线被成功收购</p>
       </div>
       <div class="member">
         <div class="name">Han Gao</div>
-        <div class="title">Co-Founder/Former Tencent Employee</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Former head of brand dept. in Beijing Miduo Financials</p>
-        <p class="desc">• Former head of brand dept. in Anbang Hexie Insurance</p>
-        <p class="desc">• Former chief editor of online public relation department in Sun Medi</p>
-        <p class="desc">• Former chief editor of online news channel in Huanqiu.com</p>
-        <p class="desc">• Worked as editor in CCTV international news, Tencent tech news</p>
+        <p class="desc">• 10年一线媒体从业经历</p>
+        <p class="desc">• 历任北京米多财富品牌部总经理、安邦和谐健康保险品牌部总经理、阳光传媒公关网络部总编辑、中央电视台新闻中心国际新闻编辑、腾讯科技网站部新闻中心核心编辑、环球时报网络部新闻频道主编兼中心主任</p>
       </div>
       <div class="member">
         <div class="name">Xun Huang</div>
-        <div class="title">Co-Founder</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• One of most accomplished sound director in China, over 15 years of experience in sound editing/ sound recording</p>
-        <p class="desc">• Works include critically acclaimed films Looper, Her and numerous other films</p>
-        <p class="desc">• Graduated from sound department at Beijing Film Academy in 2000</p>
+        <p class="desc">• 知名录音师，在上海电影集团担任电影录音师，15年电影录音从业经验</p>
+        <p class="desc">• 制作了环形使者（Looper）、云端情人（Her）等电影国内场景的录音，以及大量短片、纪录片，电影《奔跑吧兄弟》总录音师</p>
+        <p class="desc">• 2000年毕业于北京电影学院录音系</p>
       </div>
       <div class="member">
         <div class="name">Yu Lei</div>
-        <div class="title">Co-Founder</div>
+        <div class="title">联合创始人</div>
         <div class="sep"></div>
-        <p class="desc">• Former CEO if GuoruiYingdu Media, co.</p>
-        <p class="desc">• Invested and produced over 10 TV series and films</p>
-      </div>
-      <div class="member">
-        <div class="name">Steve Wu</div>
-        <div class="title">Architect/Former Tencent Employe</div>
-        <div class="sep"></div>
-        <p class="desc">• IT Specialist, worked at Tencent, Woniu</p>
-        <p class="desc">• Specialist in internet security and cryptography</p>
-        <p class="desc">• Currently responsible for architecture development in TYT</p>
-      </div>
-      <div class="member">
-        <div class="name">Lin Xie</div>
-        <div class="title">Architect/Former Tencent Employe</div>
-        <div class="sep"></div>
-        <p class="desc">• IT specialist, worked at Tencent, Alibaba, and Huawei</p>
-        <p class="desc">• One of the earliest architect in Aliyun, responsible for cloud server cluster development and maintenance</p>
-        <p class="desc">• Currently responsible for architecture development in TYT</p>
-      </div>
-      <div class="member">
-        <div class="name">Kaka Zhang</div>
-        <div class="title">Blockchain Developer/Former Tencent Employee</div>
-        <div class="sep"></div>
-        <p class="desc">• Former backend engineer in Tencent research academy, one of the framework developer for enterprise QQ</p>
-        <p class="desc">• Currently responsible for blockchain development in TYT</p>
+        <p class="desc">• 曾担任国瑞影都文化发展（北京）有限公司总经理</p>
+        <p class="desc">• 投资制作电影《别跟狗较劲》电视剧《那些年那些事》、《前妻》、《缉毒先锋》等多部影视作品</p>
       </div>
       <div class="member">
         <div class="name">Yu Zhou</div>
-        <div class="title">Operation Manager/Former Tencent Employee</div>
+        <div class="title">运营专家</div>
         <div class="sep"></div>
-        <p class="desc">• Former operation expert in Tenpay</p>
-        <p class="desc">• Over 10 years of experience in operational management in online product, digital payment, digital copyright</p>
-        <p class="desc">• Currently responsible for operation management in TYT</p>
+        <p class="desc">• 曾任职腾讯财付通运营专家</p>
+        <p class="desc">• 对区块链深入参与并深刻了解区块链运营机制，超过10年互联网项目经验，在在线支付，数字版权领域有丰富的区块链运营经验</p>
+        <p class="desc">• 目前担任 TYT 运营工作</p>
+      </div>
+      <div class="member">
+        <div class="name">Steve Wu</div>
+        <div class="title">技术专家</div>
+        <div class="sep"></div>
+        <p class="desc">• 先后就职于腾讯、蜗牛等知名互联网公司</p>
+        <p class="desc">• 网络安全和密码学方向技术专家</p>
+        <p class="desc">• 目前担任 TYT 研发工作</p>
+      </div>
+      <div class="member">
+        <div class="name">Lin Xie</div>
+        <div class="title">技术专家</div>
+        <div class="sep"></div>
+        <p class="desc">• 先后经历华为腾讯阿里，腾讯期间担任运维专家，负责服务器集群的开发维护</p>
+        <p class="desc">• 阿里云早期的架构师之一</p>
+        <p class="desc">• 熟悉区块链底层架构和业务体系</p>
+        <p class="desc">• 目前担任 TYT 研发工作</p>
+      </div>
+      <div class="member">
+        <div class="name">Kaka Zhang</div>
+        <div class="title">技术专家</div>
+        <div class="sep"></div>
+        <p class="desc">• 原腾讯研究院高级后台开发工程师</p>
+        <p class="desc">• 企业QQ的框架开发者之一</p>
+        <p class="desc">• 目前担任 TYT 研发工作</p>
       </div>
       <div class="member">
         <div class="name">Kevin Liao</div>
-        <div class="title">Architect</div>
+        <div class="title">技术专家</div>
         <div class="sep"></div>
-        <p class="desc">• Former architect in Citi Bank</p>
-        <p class="desc">• Developed quantitative trading system and risk management system individually</p>
-        <p class="desc">• Participate in multiple blockchain opensource project</p>
-        <p class="desc">• Currently responsible for architecture development in TYT</p>
+        <p class="desc">• 曾就职于花旗银行，担任核心后台架构师</p>
+        <p class="desc">• 曾独立开发量化交易系统和现金贷风控系统</p>
+        <p class="desc">• 参与过数个区块链开源项目研发，对底层架构有着深刻的理解和认知</p>
+        <p class="desc">• 目前担任 TYT 研发工作</p>
       </div>
       <div class="member">
         <div class="name">Jimmy Luo</div>
-        <div class="title">Blockchain Developer</div>
+        <div class="title">技术专家</div>
         <div class="sep"></div>
-        <p class="desc">• Years of experience in system architecture design, quantitative trading system design</p>
-        <p class="desc">• Profound knowledge in blockchain open source technology</p>
-        <p class="desc">• Expert in multiple program language such as Golang, Python, Javascript</p>
-        <p class="desc">• Currently responsible for blockchain development in TYT</p>
+        <p class="desc">• 丰富的的互联网业务系统、金融交易系统开发设计经验</p>
+        <p class="desc">• 熟悉区块链系统架构，对ETH等区块链开源技术有深刻的研究。在多个大型开源项目中参与代码贡献、担任文档维护等工作</p>
+        <p class="desc">• 精通Golang，Python，Javascript等多种开发语言</p>
+        <p class="desc">• 目前担任 TYT 研发工作</p>
       </div>
-      <div class="sec">ADVISOR</div>
+      <div class="sec">顾问</div>
       <div class="member">
         <div class="name">Mingbo Wan</div>
         <div class="sep"></div>
-        <p class="desc">• Joined Facebook in 2015 in AI/machine learning infra team, supported multiple department at Facebook</p>
-        <p class="desc">• Worked as IT specialist at Samsung, EA</p>
-        <p class="desc">• Over 20 years of experience in database, system architecture</p>
+        <p class="desc">• 资深技术专家</p>
+        <p class="desc">• 2015年进入Facebook，担任人工智能，数据库等多个部门的底层核心架构工作</p>
+        <p class="desc">• 曾在三星，EA等多家公司担任技术专家</p>
+        <p class="desc">• 在系统架构，数据库等领域有近20年的丰富经验</p>
 
       </div>
       <div class="member">
         <div class="name">Yuhua Han</div>
         <div class="sep"></div>
-        <p class="desc">• Held a joint MBA degree in Fudan University & Hongkong University</p>
-        <p class="desc">• Worked at Ubisoft Entertainment</p>
-        <p class="desc">• Expert in games, IT, communication, digital media</p>
-        <p class="desc">• Series entrepreneur, successful angle investor</p>
-        <p class="desc">• Now managing partner at Jianyu Capital</p>
+        <p class="desc">• 复旦大学和香港大学合作国际工商管理硕士（MBA）</p>
+        <p class="desc">• 曾任职全球著名的游戏开发商和发行商Ubisoft Entertainment</p>
+        <p class="desc">• 游戏、IT、通信、互联网、数字媒体领域资深专家</p>
+        <p class="desc">• 连续成功创业者，资深天使投资人，成功创办上海聚劲、上海复千，主导投资项目包括复深蓝软件股份、天合智能、英翼文化科技等，并成功为娱票儿、万合天宜等提供FA服务</p>
+        <p class="desc">• 目前担任建裕基金管理合伙人</p>
       </div>
       <div class="member">
         <div class="name">Boping Shi</div>
         <div class="sep"></div>
-        <p class="desc">• Author of &lt;Blockchain World&gt;</p>
-        <p class="desc">• Partner of Junhao Capital</p>
+        <p class="desc">•《区块链世界》作者</p>
+        <p class="desc">• 现君灏资本合伙人</p>
 
       </div>
 
